6dedbae1e06a219bc4670375790fedd63eb9e9d2
edwinans <edyansari5@gmail.com>
2021-04-14 02:01:17 +0200

    exo2 done

--- src/Split.hs
+++ src/Split.hs
@@ -1,10 +1,22 @@
 module Split where
 
 split :: Char -> String -> [String]
-split _ _ = undefined
+split del s = splitAux del s [] []
+  where
+    splitAux :: Char -> String -> String -> [String] -> [String]
+    splitAux _ [] acc res = reverse (reverse acc : res)
+    splitAux del (c : cs) acc res
+      | c == del = splitAux del cs [] (reverse acc : res)
+      | otherwise = splitAux del cs (c : acc) res
 
 unsplit :: Char -> [String] -> String
-unsplit _ _ = undefined
+unsplit del s_list = drop 1 $ foldl (\x y -> x <> [del] <> y) [] s_list
 
 prop_split_unsplit :: Char -> String -> Bool
 prop_split_unsplit c str = unsplit c (split c str) == str
+
+--- >>> split '/' "usr/home/edwin"
+-- ["usr","home","edwin"]
+
+--- >>> unsplit '/' ["usr","home","edwin"]
+-- "usr/home/edwin"
